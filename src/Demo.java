cfedda5ad738c2329d27e50464824c6c9521034a
19802 <sakina231@yahoo.com>
2022-06-08 21:23:33 -0400

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+    public static void main(String [] args){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\19802\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+}
